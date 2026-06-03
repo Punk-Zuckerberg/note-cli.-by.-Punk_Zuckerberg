5d0e427715db57a7070207f4ad07269e174918de
Punk-Zuckerberg <copyborya@gmail.com>
2026-06-03 20:38:23 +0300

    add argv command support

--- main.c
+++ main.c
@@ -1,16 +1,37 @@
 #include "note.h"
+#include <string.h>
+#include <stdlib.h>
 
-int main(){
-while (1) {
-    int choise = menu();    
-    if (choise == 1) {
-        add_note();
-    }
-    if (choise == 2) {
-        show_notes();
-    }
-    if (choise == 3) {
-        quit();
+int main(int argc, char *argv[]){
+
+    if(argc > 1){
+        if (strcmp(argv[1], "add") == 0) {
+            add_note();
+        }
+        if (strcmp(argv[1], "show") == 0) {
+            show_notes();
+        }
+        if (strcmp(argv[1], "quit") == 0) {
+            quit();
+        }
+}
+    else {
+
+    while (1) {
+        int choise = menu();
+        
+        if (choise == 1) {
+            add_note();
+                exit(0);
+        }
+        if (choise == 2) {
+            show_notes();
+                exit(0);
+        }
+        if (choise == 3) {
+            quit(); 
+    }    
+
     }
 }
     return 0;
